3ca41b36fe22ef700af4d710eefbad7177cc97b3
Nafees <nafeeshassan16@gmail.com>
2017-10-28 23:31:19 +0500

    Added some files, which I forgot to in the previous commit
    Signed-off-by: Nafees <nafeeshassan16@gmail.com>

---
+++ source/server.d
@@ -0,0 +1,28 @@
+﻿/*
+ * Server demo.
+ * Listens for incoming connections on port 2525.
+ * Accepts any connection.
+ * Any message recieved will be sent back (echo-ed).
+ */
+module server;
+
+version(serverdemo){
+	import networkd;
+	import std.stdio;
+	
+	void main(){
+		Node server = new Node(true, 2525);
+		while (true){
+			Event event = server.getEvent();
+			if (event.eventType == Event.Type.MessageEvent){
+				uint senderID = cast(uint)event.conID;
+				char[] message = event.getEventData!(Event.Type.MessageEvent);
+				writeln("Message received from ID#",senderID,':');
+				writeln(message);
+				server.sendMessage(senderID, cast(char[])"Server: "~message);
+			}else if (event.eventType == Event.Type.ConnectionAccepted){
+				writeln("New connection accepted. ConnectionID:",event.conID);
+			}
+		}
+	}
+}
